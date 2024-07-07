db818a70d9b84231741a205d88f0d0fd61162c62
sphrx <vovo4a@gmail.com>
2024-07-07 18:49:17 +0300

    lesson02 homework

--- lesson09hw_02.py
+++ lesson09hw_02.py
@@ -13,7 +13,7 @@ assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
 assert difference() == 0, 'Test5'
-print('OK')
+print('Test passed')
 
 # # Interactive input
 # "Enter numbers separated by spaces (press Enter when done):"
