100e1939f1375592e1c6b25297a0129c34ba870d
sphrx <vovo4a@gmail.com>
2024-07-25 18:14:08 +0300

    lesson02 homework

--- lesson13hw_02.py
+++ lesson13hw_02.py
@@ -1,43 +1,43 @@
 class Counter:
     def __init__(self, current=1, min_value=0, max_value=10):
+        self.current = current
         self.min_value = min_value
         self.max_value = max_value
-        self.set_current(current)
 
     def set_current(self, start):
         if self.min_value <= start <= self.max_value:
             self.current = start
         else:
-            raise ValueError("Initial value must be between minimum and maximum")
+            raise ValueError("Начальное значение должно быть между минимальным и максимальным")
 
     def set_max(self, max_max):
         if max_max >= self.min_value:
             self.max_value = max_max
         else:
-            raise ValueError("Maximum value must be greater than or equal to minimum")
+            raise ValueError("Максимальное значение должно быть больше или равно минимальному")
 
     def set_min(self, min_min):
         if min_min <= self.max_value:
             self.min_value = min_min
         else:
-            raise ValueError("Minimum value must be less than or equal to maximum")
+            raise ValueError("Минимальное значение должно быть меньше или равно максимальному")
 
     def step_up(self):
         if self.current < self.max_value:
             self.current += 1
         else:
-            raise ValueError("Maximum value reached")
+            raise ValueError("Достигнут максимум")
 
     def step_down(self):
         if self.current > self.min_value:
             self.current -= 1
         else:
-            raise ValueError("Minimum value reached")
+            raise ValueError("Достигнут минимум")
 
     def get_current(self):
         return self.current
 
-# Tests
+# Тесты
 counter = Counter()
 counter.set_current(7)
 counter.step_up()
@@ -47,7 +47,7 @@ assert counter.get_current() == 10, 'Test1'
 try:
     counter.step_up()  # ValueError
 except ValueError as e:
-    print(e)  # Maximum value reached
+    print(e)  # Достигнут максимум
 assert counter.get_current() == 10, 'Test2'
 
 counter.set_min(7)
@@ -58,5 +58,5 @@ assert counter.get_current() == 7, 'Test3'
 try:
     counter.step_down()  # ValueError
 except ValueError as e:
-    print(e)  # Minimum value reached
+    print(e)  # Достигнут минимум
 assert counter.get_current() == 7, 'Test4'
